b1615c25dc04427e7cdb2ef0350f56ee4349c9cc
ztby74 <83415876+ztby74@users.noreply.github.com>
2021-07-25 23:28:22 +0800

    Update install_2.sh

--- install_2.sh
+++ install_2.sh
@@ -1,7 +1,7 @@
   
 #!/bin/bash
 
-pacman -S networkmanager network-manager-applet nm-connection-editor grub efibootmgr os-prober dosfstools ntfs-3g mtools amd-ucode noto-fonts
+pacman -S networkmanager network-manager-applet nm-connection-editor grub efibootmgr os-prober dosfstools ntfs-3g mtools amd-ucode noto-fonts nautilus xorg-server xorg-xinit xfce4-terminal feh  compton rofi google-chrome  ttf-liberation  adobe-source-han-sans-cn-fonts  adobe-source-han-sans-tw-fonts    adobe-source-han-sans-jp-fonts
 
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB 
 
