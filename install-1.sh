33bbcc528c7d73f3cb41a49fcfb09048277a37af
ztby74 <83415876+ztby74@users.noreply.github.com>
2021-07-25 23:35:31 +0800

    Create install-1.sh

---
+++ install-1.sh
@@ -0,0 +1,33 @@
+pacman - Syy fish  openssh
+
+fish
+
+ls /sys/firmware/efi/efivars/
+
+
+timedatectl set-ntp true
+
+timeddatectl status
+date
+
+fdisk -l
+
+cfdisk /dev/sdX
+
+mkfs.ext4    /dev/sda3
+mkfs.ext4    /dev/sda4
+mkfs.vfat     /dev/sda1
+mkswap -f   /dev/sda2
+swapon        /dev/sda2
+
+mount   /dev/sda3    /mnt
+mkdir    /mnt/home
+mount   /dev/sda4/  mnt/home
+mkdir    /mnt/boot/EFI
+mount   /dev/sda1    /mnt/boot/EFI
+
+reflector   -c  China  -f  10  --save   /etc/pacman.d/mirrorlist
+
+
+
+
