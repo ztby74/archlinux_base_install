7ace5db3f3ee57abf76551e40138f570c985743e
ztby74 <83415876+ztby74@users.noreply.github.com>
2021-10-10 12:22:24 +0800

    Update install_111.sh

--- install_111.sh
+++ install_111.sh
@@ -4,3 +4,5 @@ reflector -c China -f 10 --save /etc/pacman.d/mirrorlist
 pacstrap /mnt base base-devel linux linux-firmware linux-headers vim vi neovim bash zsh fish reflector git 
 
 genfstab -U /mnt >> /mnt/etc/fstab
+
+arch-chroot /mnt
